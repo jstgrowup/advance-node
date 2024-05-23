04ad9c830a23494e0cdde6c2dfb205ee07f9d973
jstgrowup <deysubham999@gmail.com>
2024-05-23 23:34:58 +0530

    fixes

--- projectsetup/config/dev.js
+++ projectsetup/config/dev.js
@@ -1,16 +1,8 @@
 module.exports = {
   googleClientID:
-<<<<<<< HEAD
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority",
+    "mongodb+srv://nodeadvance:nodeadvance@cluster0.nnuecms.mongodb.net/nodeadvance?retryWrites=true&w=majority",
   cookieKey: "123123123",
-=======
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
-  cookieKey: '123123123',
->>>>>>> d6ab82837ebb4d138b51677db62a6524c5a32ca3
 };
