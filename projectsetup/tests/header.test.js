56ca09f82e60422ea72a4720d8613b8f83a5db0c
jstgrowup <deysubham999@gmail.com>
2024-05-26 23:26:30 +0530

    browser checks

--- projectsetup/tests/header.test.js
+++ projectsetup/tests/header.test.js
@@ -1,14 +1,26 @@
 const puppeteer = require("puppeteer");
-test("Adds two numbers", () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
-});
-test("we can launch a browser", async () => {
-  const browser = await puppeteer.launch({
+let browser, page;
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
   });
   // headless :false will make sure we see the graphical user interface
-  const page = await browser.newPage();
+  page = await browser.newPage();
   console.log("page:", page);
   await page.goto("localhost:3000");
 });
+afterEach(async () => {
+  await browser.close();
+});
+test(" header has the correct text", async () => {
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  // Okay, so hopefully that gives you a little bit better idea
+  // of what's going on behind the scenes
+  // when we make use of puppeteer.
+  // The real idea here is that all the code that we run
+  // is being serialized into text, sent over to Chromium,
+  // executed, the result gets serialized back in the text,
+  // and then sent back over to our NodeJS runtime.
+  expect(text).toEqual("Blogster");
+});
+test("clicking login starts the oauth flow", async () => {});
