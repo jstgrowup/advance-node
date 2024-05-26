35b02d5f26e8e0c7ace32e69f66d095ae1ea76b4
jstgrowup <deysubham999@gmail.com>
2024-05-26 23:42:27 +0530

    chromium

--- projectsetup/tests/header.test.js
+++ projectsetup/tests/header.test.js
@@ -6,7 +6,6 @@ beforeEach(async () => {
   });
   // headless :false will make sure we see the graphical user interface
   page = await browser.newPage();
-  console.log("page:", page);
   await page.goto("localhost:3000");
 });
 afterEach(async () => {
@@ -23,4 +22,38 @@ test(" header has the correct text", async () => {
   // and then sent back over to our NodeJS runtime.
   expect(text).toEqual("Blogster");
 });
-test("clicking login starts the oauth flow", async () => {});
+test("clicking login starts the oauth flow", async () => {
+  await page.click(".right a");
+  // https://pptr.dev/api/puppeteer.page.click
+  const url = await page.url();
+  //   Now we never quite know what list
+  // of emails are going to appear on the screen here
+  // and we also don't necessarily know
+  // that this thing is always going to say
+  // choose an account or anything like that,
+  // but there is one element
+  // of this page that I think will always be consistent
+  // and that is the URL.
+  // Not the whole URL,
+  // so if you look at the whole URL
+  // you'll notice that there's a couple
+  // of IDs and whatnot inside of it.
+  // But at the very start, you'll notice
+  // that the domain here is accounts.google.com.
+  // So I think that one way that we could assert
+  // that we properly have been kicked
+  // into our Oauth flow is by looking
+  // at the URL and writing an assertion that says that,
+  // yep, accounts.google.com is present inside the URL.
+  // We're gonna take this approach so
+  // that we don't ever have to worry
+  // about Google changing the text
+  // or anything like that on their Oauth flow
+  // 'cause if they ever change the text here
+  // that would suddenly break our test suite,
+  // which we definitely would not want to have happen.
+  console.log("url:", url);
+  // https://pptr.dev/api/puppeteer.frame.url
+  expect(url).toMatch(/accounts\.google\.com/);
+  // https://jestjs.io/docs/expect#tomatchregexp--string
+});
