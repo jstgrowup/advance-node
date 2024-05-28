0ff857ad669fbcb898b33ce0268a6f67f2040943
jstgrowup <deysubham999@gmail.com>
2024-05-28 17:25:47 +0530

    authentication test

--- projectsetup/tests/header.test.js
+++ projectsetup/tests/header.test.js
@@ -52,8 +52,30 @@ test("clicking login starts the oauth flow", async () => {
   // 'cause if they ever change the text here
   // that would suddenly break our test suite,
   // which we definitely would not want to have happen.
-  console.log("url:", url);
+
   // https://pptr.dev/api/puppeteer.frame.url
   expect(url).toMatch(/accounts\.google\.com/);
   // https://jestjs.io/docs/expect#tomatchregexp--string
+});
+test("when signed in shows logout button", async () => {
+  const id = "664f84ac7a923449a80e4ec2";
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+  await page.waitFor('a[href="/auth/logout"]');
+  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  expect(text).toEqual("Logout");
 });
