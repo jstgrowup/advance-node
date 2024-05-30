b960e9d3d30492c87ff8a3c7d974370cfdf20d7d
jstgrowup <deysubham999@gmail.com>
2024-05-30 23:41:28 +0530

    blogs tests

--- projectsetup/tests/blogs.test.js
+++ projectsetup/tests/blogs.test.js
@@ -9,4 +9,13 @@ afterEach(async () => {
 });
 test("When logged in can see blog create form", async () => {
   await page.login();
+  await page.click("a.btn-floating");
+  const label = await page.getContentsOf("form label");
+  expect(label).toEqual("Blog Title");
+});
+test("When logged in can see blog create form", async () => {
+  await page.login();
+  await page.click("a.btn-floating");
+  const label = await page.getContentsOf("form label");
+  expect(label).toEqual("Blog Title");
 });
--- projectsetup/tests/helpers/page.js
+++ projectsetup/tests/helpers/page.js
@@ -7,10 +7,8 @@ class CustomPage {
     const browser = await puppeteer.launch({
       headless: false,
     });
-
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
-
     return new Proxy(customPage, {
       get: function (target, property) {
         return customPage[property] || browser[property] || page[property];
@@ -25,7 +23,6 @@ class CustomPage {
   async login() {
     const user = await userFactory();
     const { session, sig } = sessionFactory(user);
-
     await this.page.setCookie({ name: "session", value: session });
     await this.page.setCookie({ name: "session.sig", value: sig });
     await this.page.goto("localhost:3000/blogs");
