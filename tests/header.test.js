6d5e9fe86d57cdb390a580308da3fef64aff8513
jstgrowup <deysubham999@gmail.com>
2024-06-01 22:14:50 +0530

    yml

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,17 @@
 const Page = require("./helpers/page");
-
 let page;
-
 beforeEach(async () => {
   page = await Page.build();
   await page.goto("localhost:3000");
 });
-
 afterEach(async () => {
   await page.close();
 });
-
 test("the header has the correct text", async () => {
   const text = await page.getContentsOf("a.brand-logo");
 
   expect(text).toEqual("Blogster");
 });
-
 test("clicking login starts oauth flow", async () => {
   await page.click(".right a");
 
@@ -24,7 +19,6 @@ test("clicking login starts oauth flow", async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
-
 test("When signed in, shows logout button", async () => {
   await page.login();
 
--- tests/setup.js
+++ tests/setup.js
@@ -2,7 +2,6 @@ jest.setTimeout(160000);
 require("../models/User");
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
-
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
